266e9216a4c5f37931697f5dc4bf927c4b0f7d11
Xoul <devxoul@gmail.com>
2011-12-21 21:22:12 +0900

    Initial Commit

---
+++ api/info.php
@@ -0,0 +1,109 @@
+<?php
+include 'curl.php';
+
+$song_id = $_GET['song_id'];
+$album_id = $_GET['album_id'];
+if( !isset( $song_id ) )
+{
+	echo 'Invalid song_id.';
+	return;
+}
+
+if( !isset( $album_id ) )
+{
+	echo 'Invalid album_id.';
+	return;
+}
+
+$html_lyrics = file_get_contents_curl( 'http://m.music.daum.net/song/vcliplyrics?song_id='.$song_id );
+$html_album = file_get_contents_curl( 'http://m.music.daum.net/album/main?album_id='.$album_id );
+
+$res = array();
+$title = get_title( $html_lyrics );
+$res['title'] = urlencode( $title );
+$res['artist'] = urlencode( get_artist( $html_album ) );
+$res['album'] = urlencode( get_album( $html_album ) );
+$res['cover_url'] = urlencode( get_cover_url( $html_album ) );
+$res['lyrics'] = urlencode( get_lyrics( $html_lyrics ) );
+$res['track'] = urlencode( get_track( $html_album, $title ) );
+$res['total_track'] = urlencode( get_total_track( $html_album ) );
+
+echo urldecode( json_encode( $res ) );
+
+function get_title( $src )
+{
+	$title = split( '<h4 class="tit">', $src );
+	$title = split( '
+', $title[1] );
+	return $title[0];
+}
+
+function get_artist( $src )
+{
+	$artists = split( 'class="txt_artist">', $src );
+	$len = count( $artists );
+	$artist = '';
+	for( $i = 1; $i < $len; $i++ )
+	{
+		$current_artist = split( '</a>', $artists[$i] );
+		$artist .= $current_artist[0];
+		
+		if( $i < $len - 1 )
+		{
+			$artist .= ', ';
+		}
+	}
+	
+	return $artist;
+}
+
+function get_album( $src )
+{
+	$album = split( '<h4 class="tit">', $src );
+	$album = split( '</h4>', $album[1] );
+	return $album[0];
+}
+
+function get_cover_url( $src )
+{
+	$num = split( 'http://m1.daumcdn.net/cfile', $src );
+	$num = split( '/', $num[1] );
+	$id = split( '/C94x94/', $src );
+	$id = split( '"', $id[1] );	
+	return 'http://i1.daumcdn.net/cfile'.$num[0].'/S200x200/'.$id[0];
+}
+
+function get_lyrics( $src )
+{
+	$lyrics = split( '<p class="lyrics">', $src );
+	$lyrics = split( '<br/>', $lyrics[1] );
+	return $lyrics[0];
+}
+
+function get_track( $src, $title )
+{
+	$songs = split( '<span class="num_rank">', $src );
+	$len = count( $songs );
+	for( $i = 1; $i < $len; $i++ )
+	{
+		$song_title = split( '<strong class="tit">', $songs[$i] );
+		$song_title = split( '</strong>', $song_title[1] );
+//		echo 'song_title : '.$song_title[0].'<br />title_____ : '.$title.'<br />';
+		
+//		if( $song_title[0] == $title )
+		if( strpos( $title, $song_title[0] ) > -1 )
+		{
+//			echo 'same!!<br />';
+			$track = split( '</span>', $songs[$i] );
+			return $track[0];
+		}
+	}
+	
+	return -1;
+}
+
+function get_total_track( $src )
+{
+	return count( split( '<span class="num_rank">', $src ) ) - 1;
+}
+?>
---
+++ api/search.php
@@ -0,0 +1,66 @@
+<?php
+include 'curl.php';
+
+$query = $_GET['query'];
+$page = $_GET['page'];
+if( !isset( $page ) )
+	$page = 1;
+
+$url = 'http://m.music.daum.net/search/list?type=song&query='.$query.'&page_no='.$page;
+$html = split( '<div class="wrap_cont">', file_get_contents_curl( $url ) );
+$len = count( $html) - 1;
+$res = array();
+for( $i = 0; $i < $len; $i++ )
+{
+	$res[$i]['song_id'] = get_song_id( $html[$i + 1] );
+	$res[$i]['album_id'] = get_album_id( $html[$i + 1] );
+	$res[$i]['title'] = urlencode( get_title( $html[$i + 1] ) );
+	$res[$i]['artist'] = urlencode( get_artist( $html[$i + 1] ) );
+	$res[$i]['album'] = urlencode( get_album( $html[$i + 1] ) );
+}
+
+echo urldecode( json_encode( $res ) );
+
+function get_song_id( $src )
+{
+	$song_id = split( '<div id="sId_', $src );
+	$song_id = split( '"', $song_id[1] );
+	return $song_id[0];
+}
+
+function get_album_id( $src )
+{
+	$album_id = split( 'album_id=', $src );
+	$album_id = split( '"', $album_id[1] );
+	return $album_id[0];
+}
+
+function get_title( $src )
+{
+	$title = split( '<strong class="tit">', $src );
+	$title = split( '</strong>', $title[1] );
+	$title = $title[0];
+	
+	if( strpos( $title, "<b>" ) > -1 )
+	{
+		$title = split( '<b>', $title );
+		$title = split( '</b>', $title[1] );
+		return $title[0];
+	}
+	
+	return $title;
+}
+
+function get_artist( $src )
+{
+	$artist = split( '<span class="txt_info">', $src );
+	$artist = split( '<span class="txt_bar">', $artist[1] );
+	return $artist[0];
+}
+
+function get_album( $src )
+{
+	$album = split( '</span>', $src );
+	return $album[2];
+}
+?>
